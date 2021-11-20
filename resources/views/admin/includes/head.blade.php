6ef9b2531712b74ada5feceba59ed3f4f3364241
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2021-11-21 00:07:34 +0200

    HomeCommit Menu CSS

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -15,8 +15,11 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
 <!--end::Fonts-->
 <!--begin::Global Stylesheets Bundle(used by all pages)-->
-{{-- <link href="{{ asset('admin/assets/plugins/global/plugins.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('admin/assets/css/style.bundle.rtl.css')}}" rel="stylesheet" type="text/css" /> --}}
+@if(LaravelLocalization::getCurrentLocaleDirection() == 'rtl')
+<link href="{{ asset('admin/assets/plugins/global/plugins.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('admin/assets/css/style.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
+@else
 <link href="{{ asset('admin/assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('admin/assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
+@endif
 @stack('styles')
